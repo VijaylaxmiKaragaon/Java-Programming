12db22d8a178bbb12da718523a6726e693e04416
VijaylaxmiKaragaon <vijaylaxmisk04@gmail.com>
2026-06-14 15:16:08 +0530

    Added folder

---
+++ conditions/First.java
@@ -0,0 +1,22 @@
+package com.conditions;
+
+
+public class First {
+
+	public static void main(String[] args) {
+		int marks = 90;
+		if(marks >= 85) {
+			System.out.println("distinction");
+		} else if(marks >= 60) {
+			System.out.println("first class");
+		} else if(marks >= 45) {
+			System.out.println("second class");
+		} else if(marks >= 35) {
+			System.out.println("just pass");
+		} else {
+			System.out.println("just fail");
+		}
+
+	}
+
+}
